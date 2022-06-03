f8fab7a09c667e624274dda18dd5cac135ae686f
Shilpa <parasianshilpa@gmail.com>
2022-06-02 22:25:31 -0400

    final code

---
+++ fizzbuzz/src/fizzbuzz/fizzbuzz.java
@@ -0,0 +1,25 @@
+package fizzbuzz;
+
+
+public class fizzbuzz {
+	public static void main(String[] args) {
+		printWord();
+	}
+	
+	public static void printWord () {
+		for (int i =1 ; i <= 100; i++) {
+			if (i % 15 == 0) {
+				System.out.println("Fizzbuzz");
+			}
+			else if (i % 3 ==0) {
+				System.out.println("Fizz");
+			}
+			else if (i % 5 ==0) {
+				System.out.println("Buzz");
+			}
+			else  {
+				System.out.println(i);
+			}
+		}
+	}
+}
